e23a4d72ac888bc900ba3fb981bce94775af5377
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2023-04-07 16:46:52 +0530

    feat: mindsdb login route

--- api/textSummary.js
+++ api/textSummary.js
@@ -3,6 +3,23 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
+const MindsDB = require('mindsdb-js-sdk');
+
+// Dotenv setup
+dotenv.config({ path: "../frontend/.env" });
+
+
+// MindsDB setup
+const user = {
+    user: process.env.MINDSDB_USER,
+    password: process.env.MINDSDB_PASS
+}
+
+const connectToMindsDB = async (user) => {
+    await MindsDB.default.connect(user);
+}
+
+
 // Express API setup
 const app = express();
 app.use(cors());
@@ -14,13 +31,25 @@ app.use(
 );
 app.use(bodyParser.json());
 
-// Dotenv setup
-dotenv.config({ path: "../frontend/.env" });
+
+// MindsDB setup
 
 // Base route
 app.get("/", function (req, res) {
     return res.json("Hello world!");
 });
+
+// Login route
+app.get("/login", async function (req, res) {
+    try{
+        await connectToMindsDB(user);
+        res.json("Login successful");
+    }
+    catch(error){
+        res.json(error);
+    }
+    
+})
 
 // Run the API
 const port = 3000;
